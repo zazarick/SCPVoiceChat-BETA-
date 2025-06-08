8fc3cd40f705779a3f1e3fa1bd7af13aa84c9598
zazarick <danyamalyshev2023@gmail.com>
2025-06-08 20:50:47 +0300

    Create Config.cs

---
+++ Config.cs
@@ -0,0 +1,20 @@
+using Exiled.API.Interfaces;
+using System.ComponentModel;
+
+namespace SCPVoiceChat
+{
+    public class Config : IConfig
+    {
+        [Description("Включение или выключение плагина SCPVoiceChat.")]
+        public bool IsEnabled { get; set; } = true;
+
+        [Description("Включить или выключить режим отладки.")]
+        public bool Debug { get; set; } = false;
+
+        [Description("Радиус слышимости SCP при разговоре (в метрах). Не влияет на работу, используется для информирования.")]
+        public float ScpVoiceRadius { get; set; } = 12f;
+
+        [Description("Включить только для определённых SCP (если false — для всех SCP ролей).")]
+        public bool OnlySpecificScps { get; set; } = true;
+    }
+}
